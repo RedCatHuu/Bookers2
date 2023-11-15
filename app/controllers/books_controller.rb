7a0068b2b719b1701a20ee56b0df8f0dfb1ab0ac
AtsushiSato <148026462+RedCatHuu@users.noreply.github.com>
2023-11-15 10:08:18 +0000

    [add]パーシャル及び、サイドバー完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,33 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end 
+  
+  def create
+    @book = Book.new(book_params)
+    # @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end 
+  
   def index
+    @books = Book.all
+    @book = Book.new
+    # @books = Book.find(params[:id])
+    @user = current_user
   end
 
   def show
+    @user = current_user
   end
 
   def edit
   end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end 
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def show
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def edit
   end
 
   def index
+    @book = Book.new
+    @user = current_user
   end
 end
